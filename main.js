7321b2721a2c64bca8f26f29d777755413012992
Sam Blundell <samcalblundell@googlemail.com>
2022-08-06 02:43:55 +0100

    experimenting with websocket interactions, most of this will get cleaned up

--- main.js
+++ main.js
@@ -1,3 +1,34 @@
 window.addEventListener('load', () => {
-    console.log('loading done.');
-})
+    const canvas = document.getElementById('MessageFrame');
+    const ctx = canvas.getContext('2d');
+    canvas.height = 700;
+    canvas.width = 500;
+    let canvasStart = 20;
+    ctx.font = '20px Helvetica';
+    ctx.textAlign = 'left';
+    ctx.fillStyle = 'black';
+
+    const msgInput = document.getElementById('MessageInput');
+    const sendMsg = document.getElementById('SendButton');
+
+    const pongSocket = new WebSocket('ws://localhost:9090');
+    sendMsg.onclick = () => {
+        pongSocket.send(msgInput.value);
+        writeMessage(ctx, msgInput.value, canvasStart);
+        msgInput.value = '';
+    }
+
+    pongSocket.addEventListener('open', (event) => {
+        pongSocket.send('Hello Server!');
+    });
+
+    const writeMessage = (context, msg, x) => {
+        context.fillText(msg, 10, x);
+        canvasStart += 25;
+    }
+
+    pongSocket.addEventListener('message', (event) => {
+        console.log('Message from server ', event.data);
+        writeMessage(ctx, event.data, canvasStart);
+    });
+});
