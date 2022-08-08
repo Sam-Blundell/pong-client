b90f264862c2a6b971db62d4b3746482e6a0656e
Sam Blundell <samcalblundell@googlemail.com>
2022-08-08 02:15:42 +0100

    add logic for handling connections and disconnections to socket class, refactor to new server parsing

--- socket.js
+++ socket.js
@@ -1,21 +1,23 @@
 export default class ServerConnection {
     constructor(game) {
         this.game = game;
+        this.url = 'ws://localhost:9090';
         this.connected = false;
-        this.socket = new WebSocket(this.game.url);
+        this.socket = new WebSocket(this.url);
         this.handShake();
         this.gameStateListener();
+        this.disconnectListener();
     }
     sendInput(input, action) {
         const data = JSON.stringify({
-            inputData: [input, action]
+            inputData: [this.game.userType, input, action]
         });
         this.socket.send(data);
     }
     handShake() {
         this.socket.addEventListener('open', () => {
             this.socket.send(JSON.stringify({
-                initialState: this.game.gameState,
+                userType: this.game.userType,
             }));
             this.connected = true;
         });
@@ -26,6 +28,17 @@ export default class ServerConnection {
             if ('gameState' in parsedData) {
                 this.game.gameState = parsedData.gameState;
             }
+            if ('opponentConnected' in parsedData) {
+                this.game.opponentConnected = true;
+            }
+            if ('opponentDisconnected' in parsedData) {
+                this.game.opponentConnected = false;
+            }
+        })
+    }
+    disconnectListener() {
+        this.socket.addEventListener('close', (event) => {
+            this.connected = false;
         })
     }
 }
