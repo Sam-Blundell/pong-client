798652bc908b498c7cd19b697190f4df7bd60f4c
Sam Blundell <samcalblundell@googlemail.com>
2022-08-09 01:25:09 +0100

    refactor socket file to reflect server changes

--- socket.js
+++ socket.js
@@ -1,30 +1,29 @@
 export default class ServerConnection {
     constructor(game) {
         this.game = game;
-        this.url = 'ws://localhost:9090';
         this.connected = false;
-        this.socket = new WebSocket(this.url);
+        this.socket = new WebSocket(this.game.url);
         this.handShake();
         this.gameStateListener();
         this.disconnectListener();
     }
     sendInput(input, action) {
         const data = JSON.stringify({
-            inputData: [this.game.userType, input, action]
+            inputData: [input, action]
         });
         this.socket.send(data);
     }
     handShake() {
         this.socket.addEventListener('open', () => {
-            this.socket.send(JSON.stringify({
-                userType: this.game.userType,
-            }));
             this.connected = true;
         });
     }
     gameStateListener() {
         this.socket.addEventListener('message', (event) => {
             const parsedData = JSON.parse(event.data);
+            if ('gameID' in parsedData) {
+                this.game.gameID = parsedData.gameID;
+            }
             if ('gameState' in parsedData) {
                 this.game.gameState = parsedData.gameState;
             }
